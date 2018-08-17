aed81c268fa3612b46608f49a05bad17b97bf6f6
Jeliozver <jelioradoev@gmail.com>
2018-08-17 21:01:11 +0300

    added methods to the book service

--- client/src/app/core/services/book.service.ts
+++ client/src/app/core/services/book.service.ts
@@ -13,6 +13,12 @@ import { Book } from '../models/book.model';
 
 const domain = 'http://localhost:8000';
 const getSingleBookEndpoint = domain + '/book/details/';
+const createBookEndpoint = domain + '/book/add';
+const editBookEndpoint = domain + '/book/edit/';
+const deleteBookEndpoint = domain + '/book/delete/';
+const rateBookEndpoint = domain + '/book/rate/';
+const addToFavoritesEndpoint = domain + '/book/addToFavorites/';
+const searchBookEndpoint = domain + '/book/search';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +29,29 @@ export class BookService {
 
   getSingleBook(id: string): Observable<ServerResponse<Book>> {
     return this.http.get<ServerResponse<Book>>(getSingleBookEndpoint + id);
+  }
+
+  createBook(payload: Book): Observable<ServerResponse<Book>> {
+    return this.http.post<ServerResponse<Book>>(createBookEndpoint, payload);
+  }
+
+  editBook(id: string, payload: Book): Observable<ServerResponse<Book>> {
+    return this.http.put<ServerResponse<Book>>(editBookEndpoint + id, payload);
+  }
+
+  deleteBook(id: string): Observable<ServerResponse<Book>> {
+    return this.http.delete<ServerResponse<Book>>(deleteBookEndpoint + id);
+  }
+
+  rateBook(id: string, payload: object): Observable<ServerResponse<Book>> {
+    return this.http.post<ServerResponse<Book>>(rateBookEndpoint + id, payload);
+  }
+
+  addToFavourites(id: string): Observable<ServerResponse<Book>> {
+    return this.http.post<ServerResponse<Book>>(addToFavoritesEndpoint + id, {});
+  }
+
+  searchBook(query: string): Observable<ServerResponse<Book[]>> {
+    return this.http.get<ServerResponse<Book[]>>(searchBookEndpoint + query);
   }
 }
