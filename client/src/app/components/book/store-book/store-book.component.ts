1068e9c19e7cfd28fda3cf33d4d0ef4fd25e6211
Jeliozver <jelioradoev@gmail.com>
2018-08-17 13:50:50 +0300

    added book module, book service and book related components

---
+++ client/src/app/components/book/store-book/store-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-book',
+  templateUrl: './store-book.component.html',
+  styleUrls: ['./store-book.component.css']
+})
+export class StoreBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
