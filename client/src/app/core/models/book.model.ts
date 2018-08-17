f81513583d3b57d1e8b0b21ecc28d2b3528a3ba0
Jeliozver <jelioradoev@gmail.com>
2018-08-17 20:04:21 +0300

    added book model

---
+++ client/src/app/core/models/book.model.ts
@@ -0,0 +1,13 @@
+export class Book {
+  constructor(
+    title: 'Alex Ferguson: My Biography',
+    author: 'Sir Alex Ferguson',
+    genre: 'Autobiography',
+    year: '2014',
+    description: '',
+    cover: 'https://i.imgur.com/JCXkMpY.jpg',
+    isbn: '034091940X',
+    pagesCount: '416',
+    price: '11.96'
+  ) { }
+}
