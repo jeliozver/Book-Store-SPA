acc87c0162ccc7f7d78d56e7905133674db98faf
Jeliozver <jelioradoev@gmail.com>
2018-08-17 16:08:00 +0300

    minor tweaks

--- client/src/app/core/shared/shared.module.ts
+++ client/src/app/core/shared/shared.module.ts
@@ -2,6 +2,12 @@
 import { NgModule } from '@angular/core';
 
 // Modules
+// Modules imports here
+
+// Components
+// Components imports here
+
+// Directives
 import { MustMatchDirective } from './must-match.directive';
 
 @NgModule({
