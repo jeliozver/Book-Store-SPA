5bfbe037c07774530e2f1b0059074a3aa9a9d6d1
Jeliozver <jelioradoev@gmail.com>
2018-08-18 16:51:31 +0300

    minor tweaks

--- server/config/routes.js
+++ server/config/routes.js
@@ -18,7 +18,7 @@ module.exports = (APP) => {
     APP.post('/user/cart/checkout', AUTH.isAuth, CART_CONTROLLER.checkout);
 
     APP.get('/book/search', BOOK_CONTROLLER.search);
-    APP.get('/book/:bookId', BOOK_CONTROLLER.getSingle);
+    APP.get('/book/details/:bookId', BOOK_CONTROLLER.getSingle);
     APP.post('/book/add', AUTH.isInRole('Admin'), BOOK_CONTROLLER.add);
     APP.put('/book/edit/:bookId', AUTH.isInRole('Admin'), BOOK_CONTROLLER.edit);
     APP.delete('/book/delete/:bookId', AUTH.isInRole('Admin'), BOOK_CONTROLLER.delete);
