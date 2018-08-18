196b847bd722c44f7a103bc2dcb8db7dffe4d1bf
Jeliozver <jelioradoev@gmail.com>
2018-08-18 15:28:17 +0300

    added create book functionality

--- client/src/app/components/book/book-create/book-create.component.ts
+++ client/src/app/components/book/book-create/book-create.component.ts
@@ -1,15 +1,121 @@
-import { Component, OnInit } from '@angular/core';
+// Decorators and Lifehooks
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+// Forms
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+
+// Router
+import { Router } from '@angular/router';
+
+// RXJS
+import { Subscription } from 'rxjs';
+
+// Services
+import { BookService } from '../../../core/services/book.service';
+
+// Custom Validators
+import { isUrlValidator } from '../../../core/shared/is-url.directive';
+import { isIsbnValidator } from '../../../core/shared/is-isbn.directive';
 
 @Component({
   selector: 'app-book-create',
   templateUrl: './book-create.component.html',
   styleUrls: ['./book-create.component.css']
 })
-export class BookCreateComponent implements OnInit {
+export class BookCreateComponent implements OnInit, OnDestroy {
+  createBookForm: FormGroup;
+  createBookSub$: Subscription;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private bookService: BookService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createBookForm = new FormGroup({
+      'title': new FormControl('', [
+        Validators.required
+      ]),
+      'author': new FormControl('', [
+        Validators.required
+      ]),
+      'genre': new FormControl('', [
+        Validators.required
+      ]),
+      'year': new FormControl('', [
+        Validators.required
+      ]),
+      'description': new FormControl('', [
+        Validators.required,
+        Validators.minLength(10)
+      ]),
+      'cover': new FormControl('', [
+        Validators.required,
+        isUrlValidator
+      ]),
+      'isbn': new FormControl('', [
+        Validators.required,
+        isIsbnValidator
+      ]),
+      'pagesCount': new FormControl('', [
+        Validators.required,
+        Validators.min(0)
+      ]),
+      'price': new FormControl('', [
+        Validators.required,
+        Validators.min(0)
+      ])
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.createBookSub$) {
+      this.createBookSub$.unsubscribe();
+    }
+  }
+
+  onSubmit(): void {
+    this.createBookSub$ = this.bookService
+      .createBook(this.createBookForm.value)
+      .subscribe(() => {
+        this.router.navigate(['/home']);
+      });
+  }
+
+  get title(): AbstractControl {
+    return this.createBookForm.get('title');
+  }
+
+  get author(): AbstractControl {
+    return this.createBookForm.get('author');
+  }
+
+  get genre(): AbstractControl {
+    return this.createBookForm.get('genre');
+  }
+
+  get year(): AbstractControl {
+    return this.createBookForm.get('year');
+  }
+
+  get description(): AbstractControl {
+    return this.createBookForm.get('description');
+  }
+
+  get cover(): AbstractControl {
+    return this.createBookForm.get('cover');
+  }
+
+  get isbn(): AbstractControl {
+    return this.createBookForm.get('isbn');
+  }
+
+  get pagesCount(): AbstractControl {
+    return this.createBookForm.get('pagesCount');
+  }
+
+  get price(): AbstractControl {
+    return this.createBookForm.get('price');
   }
 
 }
