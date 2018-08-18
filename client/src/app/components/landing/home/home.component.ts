a277103090a841d134e69f4e78f9193f60e8ba92
Jeliozver <jelioradoev@gmail.com>
2018-08-18 10:59:27 +0300

    added getting books functionality for home component

--- client/src/app/components/landing/home/home.component.ts
+++ client/src/app/components/landing/home/home.component.ts
@@ -1,23 +1,56 @@
 // Decorators and Lifehooks
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 // Services
 import { BookService } from '../../../core/services/book.service';
 
 // Models
 import { Book } from '../../../core/models/book.model';
+import { Subscription } from 'rxjs';
+
+const newestBooksQuery = '?sort={"creationDate":-1}&limit=6';
+const bestRatedBooksQuery = '?sort={"currentRating":-1}&limit=6';
+const mostPurchasedBooksQuery = '?sort={"purchasesCount":-1}&limit=6';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  newestSub$: Subscription;
+  bestRatedSub$: Subscription;
+  mostPurchasedSub$: Subscription;
+  newestBooks: Book[];
+  bestRatedBooks: Book[];
+  mostPurchasedBooks: Book[];
 
   constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
+    this.newestSub$ = this.bookService
+      .search(newestBooksQuery)
+      .subscribe((res) => {
+        this.newestBooks = res.data;
+      });
 
+    this.bestRatedSub$ = this.bookService
+      .search(bestRatedBooksQuery)
+      .subscribe((res) => {
+        this.bestRatedBooks = res.data;
+      });
+
+    this.mostPurchasedSub$ = this.bookService
+      .search(mostPurchasedBooksQuery)
+      .subscribe((res) => {
+        this.mostPurchasedBooks = res.data;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.newestSub$.unsubscribe();
+    this.bestRatedSub$.unsubscribe();
+    this.mostPurchasedSub$.unsubscribe();
   }
 
 }
