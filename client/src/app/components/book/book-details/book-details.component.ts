107771bdfb80fd77f993884f09f2dfe196f5748e
Jeliozver <jelioradoev@gmail.com>
2018-08-20 18:23:18 +0300

    added book details initial setup

--- client/src/app/components/book/book-details/book-details.component.ts
+++ client/src/app/components/book/book-details/book-details.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+// Decorators and Lifehooks
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+// Router
+import { ActivatedRoute } from '@angular/router';
+
+// RXJS
+import { Subscription } from 'rxjs';
+
+// Services
+import { BookService } from '../../../core/services/book.service';
+import { CommentService } from '../../../core/services/comment.service';
+import { CartService } from '../../../core/services/cart.service';
+import { HelperService } from '../../../core/services/helper.service';
+
+// Models
+import { Book } from '../../../core/models/book.model';
+import { Comment } from '../../../core/models/comment.model';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent implements OnInit, OnDestroy {
+  bookSub$: Subscription;
+  book: Book;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService,
+    private commentService: CommentService,
+    private cartService: CartService,
+    private helperService: HelperService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
