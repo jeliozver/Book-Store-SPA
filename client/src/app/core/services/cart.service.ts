5124503b9957a2f98a5287a4e88d5d0a322d892e
Jeliozver <jelioradoev@gmail.com>
2018-08-21 13:45:32 +0300

    added functionality for crud operations

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,10 +1,40 @@
 // Decorators
 import { Injectable } from '@angular/core';
 
+// RXJS
+import { Observable } from 'rxjs';
+
+// HTTP
+import { HttpClient } from '@angular/common/http';
+
+// Models
+import { ServerResponse } from '../models/server-response.model';
+
+const baseUrl = 'http://localhost:8000/user/cart';
+const addToCartEndpoint = '/add/';
+const removeFromCartEndpoint = '/delete/';
+const checkoutEndpoint = '/checkout';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCart(): Observable<ServerResponse<object>> {
+    return this.http.get<ServerResponse<object>>(baseUrl);
+  }
+
+  addToCart(id: string): Observable<ServerResponse<object>> {
+    return this.http.post<ServerResponse<object>>(baseUrl + addToCartEndpoint + id, {});
+  }
+
+  removeFromCart(id: string): Observable<ServerResponse<object>> {
+    return this.http.delete<ServerResponse<object>>(baseUrl + removeFromCartEndpoint + id);
+  }
+
+  checkout(payload: object): Observable<ServerResponse<object>> {
+    return this.http.post<ServerResponse<object>>(baseUrl + checkoutEndpoint, payload);
+  }
 }
