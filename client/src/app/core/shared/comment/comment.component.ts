15186d72e447a4d30bb5e3a90a3512a1e6c16369
Jeliozver <jelioradoev@gmail.com>
2018-08-21 15:05:37 +0300

    moved comments interations to comment component

--- client/src/app/core/shared/comment/comment.component.ts
+++ client/src/app/core/shared/comment/comment.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+// Decorators and Lifehooks
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+
+// RXJS
+import { Subscription } from 'rxjs';
+
+// Services
+import { CommentService } from '../../services/comment.service';
+
+// Models
+import { Comment } from '../../models/comment.model';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
 })
-export class CommentComponent implements OnInit {
+export class CommentComponent implements OnInit, OnDestroy {
+  @Input('bookId') bookId: string;
+  commentsGetSub$: Subscription;
+  comments: Comment[];
+  currentPage = 1;
 
-  constructor() { }
+  constructor(private commentService: CommentService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.commentsGetSub$ = this.commentService
+      .getComments(this.bookId, (this.currentPage - 1).toString())
+      .subscribe((res) => {
+        this.comments = res.data;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.commentsGetSub$.unsubscribe();
   }
 
 }
