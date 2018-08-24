b02b258e7e384fe1695e3174bae0a5b627b45648
Jeliozver <jelioradoev@gmail.com>
2018-08-24 20:37:59 +0300

    added functionality to the component

--- client/src/app/components/user/profile/profile.component.ts
+++ client/src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,15 @@
+// Decorators and Lifehooks
 import { Component, OnInit } from '@angular/core';
+
+// Router
+import { ActivatedRoute } from '@angular/router';
+
+// Services
+import { UserService } from '../../../core/services/user.service';
+import { HelperService } from '../../../core/services/helper.service';
+
+// Models
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +17,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private helperService: HelperService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    let username = this.route.snapshot.paramMap.get('username');
+
+    if (username === 'mine') {
+      username = this.helperService.getProfile().username;
+    }
+
+    this.userService
+      .getProfile(username)
+      .subscribe((res) => {
+        this.user = res.data;
+        console.log(this.user);
+      });
+  }
+
+  blockComments(id: string): void {
+    this.userService
+      .blockComments(id)
+      .subscribe();
+  }
+
+  unblockComments(id: string): void {
+    this.userService
+      .unblockComments(id)
+      .subscribe();
   }
 
 }
