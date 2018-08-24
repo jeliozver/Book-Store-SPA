f9a605cdac2508953bde5ec9467a9b3e0b95154e
Jeliozver <jelioradoev@gmail.com>
2018-08-24 19:02:02 +0300

    minor tweask

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -135,7 +135,7 @@ module.exports = {
                 };
 
                 return res.status(200).json({
-                    message: 'User profile retreived successfully',
+                    message: '',
                     data: userToSend
                 });
             })
