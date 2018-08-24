0d5362fc29ef581846374328bc3a5c936c2c8bd4
Jeliozver <jelioradoev@gmail.com>
2018-08-24 20:31:10 +0300

    added more functionality

--- client/src/app/core/services/user.service.ts
+++ client/src/app/core/services/user.service.ts
@@ -9,10 +9,14 @@ import { HttpClient } from '@angular/common/http';
 
 // Models
 import { ServerResponse } from '../models/server-response.model';
+import { User } from '../models/user.model';
 
-const domain = 'http://localhost:8000';
-const registerEndpoint = domain + '/user/register';
-const loginEndpoint = domain + '/user/login';
+const baseUrl = 'http://localhost:8000/user';
+const registerEndpoint = baseUrl + '/register';
+const loginEndpoint = baseUrl + '/login';
+const profileEndpoint = baseUrl + '/profile/';
+const blockCommentsEndpoint = baseUrl + '/blockComments/';
+const unblockCommentsEndpoint = baseUrl + '/unlockComments/';
 
 @Injectable({
   providedIn: 'root'
@@ -21,11 +25,23 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  register(payload: object): Observable<ServerResponse<string>> {
-    return this.http.post<ServerResponse<string>>(registerEndpoint, payload);
+  register(payload: object): Observable<ServerResponse<User>> {
+    return this.http.post<ServerResponse<User>>(registerEndpoint, payload);
   }
 
-  login(payload: object): Observable<ServerResponse<string>> {
-    return this.http.post<ServerResponse<string>>(loginEndpoint, payload);
+  login(payload: object): Observable<ServerResponse<User>> {
+    return this.http.post<ServerResponse<User>>(loginEndpoint, payload);
+  }
+
+  getProfile(username: string): Observable<ServerResponse<User>> {
+    return this.http.get<ServerResponse<User>>(profileEndpoint + username);
+  }
+
+  blockComments(id: string): Observable<ServerResponse<object>> {
+    return this.http.post<ServerResponse<object>>(blockCommentsEndpoint + id, {});
+  }
+
+  unblockComments(id: string): Observable<ServerResponse<object>> {
+    return this.http.post<ServerResponse<object>>(unblockCommentsEndpoint + id, {});
   }
 }
