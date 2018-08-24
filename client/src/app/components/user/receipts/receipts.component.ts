731cec6637db52ff8a7f93e99b3e8235dd7aed6e
Jeliozver <jelioradoev@gmail.com>
2018-08-24 23:46:35 +0300

    added receipts component

---
+++ client/src/app/components/user/receipts/receipts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipts',
+  templateUrl: './receipts.component.html',
+  styleUrls: ['./receipts.component.css']
+})
+export class ReceiptsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
